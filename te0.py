1d24009ec9f26e88f25bc3ced5baf9152afd3f9b
T.Ido <hakuginhi11414@gmail.com>
2025-05-05 14:47:16 +0900

    プリントで確認

--- te0.py
+++ te0.py
@@ -1,2 +1,16 @@
-a = input()
-print(a)
+print("自然数を入力してください")
+number = int(input("ここに入力"))
+safe ="true"
+if number > 1:
+    for x in range(number-1):
+        print(x)
+        if x > 1 and number % (x + 1) == 0:
+            safe ="false"
+            
+elif number == 1:
+    safe = "false"
+
+if safe =="true":
+    print("素数")
+else:
+    print("素数ではない")
