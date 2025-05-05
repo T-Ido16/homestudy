129727f8a8a10873bb2f710f75ef31a0f470724f
T.Ido <hakuginhi11414@gmail.com>
2025-05-05 14:53:07 +0900

    １以下では弾く

--- te0.py
+++ te0.py
@@ -1,16 +1,18 @@
 print("自然数を入力してください")
-number = int(input("ここに入力"))
+number = int(input("入力された自然数を素数か判断します"))
 safe ="true"
 if number > 1:
     for x in range(number-1):
-        print(x)
         if x > 1 and number % (x + 1) == 0:
             safe ="false"
             
 elif number == 1:
     safe = "false"
+elif number < 1:
+    print("自然数ではありません")
+    safe = "Other"
 
 if safe =="true":
     print("素数")
-else:
+elif safe == "false":
     print("素数ではない")
